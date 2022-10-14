d88c3e086dd6e822bf20452b86b690a9a8c055c8
JustABeginning <jab.askme@gmail.com>
2022-10-14 19:14:51 +0530

    New Changes

--- Determinant/Determinant.java
+++ Determinant/Determinant.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 class Determinant {
-    public static int det(int[][] mat) {
+    private static int det(int[][] mat) {
         int n = mat.length;
         if (n == 1)
             return mat[0][0];
@@ -15,7 +18,7 @@ class Determinant {
         }
     }
 
-    public static int[][] minor(int[][] mat, int ri, int cj) {
+    private static int[][] minor(int[][] mat, int ri, int cj) {
         int n = mat.length - 1, r = 0, c = 0;
         int[][] arr = new int[n][n];
         for (int i = 0; i <= n; i++) {
@@ -33,16 +36,23 @@ class Determinant {
         return arr;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.print("\nEnter the ORDER of MATRIX = ");
-        int n = sc.nextInt();
-        int[][] mat = new int[n][n];
-        System.out.println("\nEnter the ELEMENT(s) :\n");
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                mat[i][j] = sc.nextInt();
-        System.out.println("\nDETERMINANT = " + det(mat));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cont = 1;
+        do {
+            System.out.print("\nEnter the ORDER of MATRIX = ");
+            int n = Integer.parseInt(br.readLine());
+            int[][] mat = new int[n][n];
+            System.out.println("\nEnter the ELEMENT(s) :\n");
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    mat[i][j] = sc.nextInt();
+            System.out.println("\nDETERMINANT = " + det(mat));
+            System.out.print("\nEnter 1 to CONTINUE : ");
+            cont = Integer.parseInt(br.readLine());
+        } while (cont == 1);
+        br.readLine();
         sc.close();
     }
 }
